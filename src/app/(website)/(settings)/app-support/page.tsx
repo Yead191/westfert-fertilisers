5cffd973a9cf473291a3e2977237dd0ef2dd4c7e
Yead191 <yead191@gmail.com>
2025-06-19 14:24:27 +0600

    added app support

---
+++ src/app/(website)/(settings)/app-support/page.tsx
@@ -0,0 +1,12 @@
+import AppSupport from "@/components/web-pages/settings/app-support";
+import React from "react";
+
+const page = () => {
+  return (
+    <section>
+      <AppSupport />
+    </section>
+  );
+};
+
+export default page;
