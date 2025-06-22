d386fdc654edfc4367194f84b84750c89a5fdeef
Yead191 <yead191@gmail.com>
2025-06-22 14:59:22 +0600

    initial deploy

--- src/redux/base/baseApi.ts
+++ src/redux/base/baseApi.ts
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
     credentials: "include",
   }),
   tagTypes: ["profile"],
